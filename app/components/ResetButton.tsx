36e5fa1983e9648dfbccd71ad583cefac825afef
Skeeter Cathcart <skeetercathcart@gmail.com>
2024-01-23 20:59:35 -0600

    buttons be random i guess

--- app/components/ResetButton.tsx
+++ app/components/ResetButton.tsx
@@ -3,22 +3,38 @@
 import { useState, useEffect } from "react";
 
 
+
 export default function ResetButton() {
 
     const[count, setCount] = useState(0)
     const[showReset, Reset] = useState(true)
-    const[buttonSize, setSize] = useState(72)
-    
-    
+   
+    //                                     Left Side                            Right Side
+    const buttonNames = ['relative w-16 h-16 bg-red-500 right-96 top-1/2', 'relative w-16 h-16 bg-red-500 -right-96 top-1/2', 
+                         'relative w-32 h-32 bg-red-500 right-96 top-1/2', 'relative w-32 h-32 bg-red-500 -right-96 top-1/2',
+                         'relative w-48 h-48 bg-red-500 right-96 top-1/2', 'relative w-48 h-48 bg-red-500 -right-96 top-1/2',
+                         'relative w-64 h-64 bg-red-500 right-96 top-1/2', 'relative w-64 h-64 bg-red-500 -right-96 top-1/2',
+                         'relative w-16 h-16 bg-red-500 right-40 top-1/2', 'relative w-16 h-16 bg-red-500 -right-40 top-1/2', 
+                         'relative w-32 h-32 bg-red-500 right-40 top-1/2', 'relative w-32 h-32 bg-red-500 -right-40 top-1/2',
+                         'relative w-48 h-48 bg-red-500 right-40 top-1/2', 'relative w-48 h-48 bg-red-500 -right-40 top-1/2',
+                         'relative w-64 h-64 bg-red-500 right-40 top-1/2', 'relative w-64 h-64 bg-red-500 -right-40 top-1/2',
+                         'relative w-16 h-16 bg-red-500 right-56 top-1/2', 'relative w-16 h-16 bg-red-500 -right-56 top-1/2', 
+                         'relative w-32 h-32 bg-red-500 right-56 top-1/2', 'relative w-32 h-32 bg-red-500 -right-56 top-1/2',
+                         'relative w-48 h-48 bg-red-500 right-56 top-1/2', 'relative w-48 h-48 bg-red-500 -right-56 top-1/2',
+                         'relative w-64 h-64 bg-red-500 right-56 top-1/2', 'relative w-64 h-64 bg-red-500 -right-56 top-1/2',
+                         'relative w-16 h-16 bg-red-500 right-72 top-1/2', 'relative w-16 h-16 bg-red-500 -right-72 top-1/2', 
+                         'relative w-32 h-32 bg-red-500 right-72 top-1/2', 'relative w-32 h-32 bg-red-500 -right-72 top-1/2',
+                         'relative w-48 h-48 bg-red-500 right-72 top-1/2', 'relative w-48 h-48 bg-red-500 -right-72 top-1/2',
+                         'relative w-64 h-64 bg-red-500 right-72 top-1/2', 'relative w-64 h-64 bg-red-500 -right-72 top-1/2']
+  
    
     return (
        
-    <div className = 'absolute inset-x-1/2 inset-y-1/2'>
-        <div className = 'flex absolute w-32 h-24 bg-slate-700 text-white items-center text-center justify-center -inset-y-72 -inset-x-6'>Number of Trials: {count}</div>
-        <button onClick = {() => Reset(false)} className = {showReset == true ? 'absolute w-16 h-16 border-2 border-red-400 bg-blue-400 justify-center text-center items-center':'hidden' }>Hello</button>
-        <button onClick = {() => {setCount(count + 1); Reset(true); setSize(80)}} className = {showReset == false && buttonSize == 72 ? 'relative w-16 h-16 border-2 border-green-600 bg-red-400 right-72' : 'hidden'}>World</button>
-        <button onClick = {() => {setCount(count + 1); Reset(true); setSize(72)}} className = {showReset == false && buttonSize == 80 ? 'relative w-16 h-16 border-2 border-green-600 bg-red-400 -right-80' : 'hidden'}>World</button>
-    </div>
+    <div className = 'flex absolute w-1/2 h-1/2 top-1/4 left-1/4 justify-center bg-green-400'>
+        <div className = 'flex absolute w-32 h-24 bg-slate-700 text-white items-center text-center justify-center'>Number of Trials: {count}</div>
+        <button onClick = {() => Reset(false)} className = {showReset == true ? 'absolute w-16 h-16 border-2 border-red-400 bg-blue-400 justify-center text-center items-center top-1/2':'hidden' }>Hello</button>
+        <button onClick = {() => {setCount(count + 1); Reset(true)}} className = {showReset == false ? `${buttonNames[count % 32]}` : 'hidden'}>World</button>
+       </div>
    
       
     );
