3b70ae21bd24c88b96f95ce4ce80fe90a3fd765b
Skeeter Cathcart <skeetercathcart@gmail.com>
2024-02-07 18:27:47 -0600

    changed trial to be its own page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-import ResetButton from "./components/ResetButton";
-import TimerDisplay from "./components/TimerDisplay";
+import Link from "next/link";
 import StopWatch from "./components/Stopwatch";
 
 export default function Home() {
   return (
     <div className = 'absolute h-full w-full bg-slate-200'>
-      <StopWatch></StopWatch>
+      <Link href = 'testing' className = 'w-24 h-12 bg-emerald-600'>Agree</Link>
     </div>
    
   );
