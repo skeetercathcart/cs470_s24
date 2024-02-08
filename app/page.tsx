7efbf4957fcfa04d2ac1e6ccbf060e36e3ff8b03
Skeeter Cathcart <skeetercathcart@gmail.com>
2024-02-08 14:41:17 -0600

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Link from "next/link";
-import StopWatch from "./components/Stopwatch";
+import { useState } from "react";
+import { pdfjs, Document, Page } from 'react-pdf';
+
+
 
 export default function Home() {
+
+
   return (
     <div className = 'absolute h-full w-full bg-slate-200'>
+      <iframe src = "/PDFs/104%NoCut.pdf" width = '100%'></iframe>
       <Link href = 'testing' className = 'w-24 h-12 bg-emerald-600'>Agree</Link>
     </div>
    
